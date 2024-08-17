3c31e994aa0344e48204bdf6e4333a50dd0e2f4b
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2024-08-17 15:44:59 +0300

    Create Diamond.cs
    Diamond

---
+++ Assets/Scripts/Diamond.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(50 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<AudioManager>().PlaySound("PickUpCoin");
+            PlayerManager.diamondCount += 1;
+            Destroy(gameObject);
+        }
+    }
+}
